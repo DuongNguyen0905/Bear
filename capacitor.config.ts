f2f2b4a23449db787f9fdbe10398dcbcff568a1c
DuongNguyen0905 <168612793+DuongNguyen0905@users.noreply.github.com>
2026-06-11 16:27:41 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.duongnguyen0905.sotay',
+  appName: ' SoTay',
+  webDir: 'dist'
+};
+
+export default config;
